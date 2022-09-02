73a2c8cd7c67d5381d5eb61d24e8a5b37e32ec4f
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-09-02 22:08:56 +0000

    Add code for first working version of LinkedList

--- 03. LinkedList/linkedlist.h
+++ 03. LinkedList/linkedlist.h
@@ -1,9 +1,66 @@
 #pragma once
 
+#include <sstream>
+
 template<typename T>
 class LinkedList {
 
 public:
+
+    // Complexity: O(1)
+    // Default constructor
+    LinkedList()
+    {
+
+    }
+
+    // Complexity: O(N)
+    ~LinkedList()
+    {
+        Node* current = _start;
+
+        while (current) {
+            Node* p = current;
+            current = current->next;
+            delete p;
+        }
+    }
+
+    // Complexity: O(1)
+    void insert_front(T value)
+    {
+        Node* new_node = new Node;
+        new_node->value = value;
+        new_node->next = _start;
+        _start = new_node;
+        ++_size;
+    }
+
+    // Complexity: O(1)
+    int size() const
+    {
+        return _size;
+    }
+
+    // Complexity: O(N)
+    std::string to_string() const
+    {
+        std::ostringstream result;
+        result << "[";
+        bool first_time = true;
+        Node* p = _start;
+        while (p) {
+            if (first_time) {
+                first_time = false;
+            } else {
+                result << ", ";
+            }
+            result << p->value;
+            p = p->next;
+        }
+        result << "]";
+        return result.str();
+    }
 
 private:
 
@@ -12,4 +69,6 @@ private:
         Node* next;
     };
 
+    Node* _start = nullptr;
+    int _size = 0;
 };
--- 03. LinkedList/main.cpp
+++ 03. LinkedList/main.cpp
@@ -3,5 +3,12 @@
 
 int main()
 {
+    LinkedList<int> my_list;
+    my_list.insert_front(7);
+    my_list.insert_front(10);
+    my_list.insert_front(3);
+
+    std::cout << my_list.to_string() << "\n";
+
     return 0;
 }
