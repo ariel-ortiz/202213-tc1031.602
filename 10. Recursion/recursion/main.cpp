c245ba1cb2dcd66ed546033be714777fa5c9cdab
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-11-16 23:03:20 +0000

    Add code to solve duplicate, last, but_last, maximum, append, repeat and merge

--- 10. Recursion/recursion/main.cpp
+++ 10. Recursion/recursion/main.cpp
@@ -17,6 +17,13 @@ int main()
     std::cout << size(a) << "\n";
     std::cout << cons_end(42, a) << "\n";
     std::cout << sum(a) << "\n";
+    std::cout << duplicate(a) << "\n";
+    std::cout << last(a) << "\n";
+    std::cout << but_last(a) << "\n";
+    std::cout << maximum(IntList {5, -1, 10, 2, 8}) << "\n";
+    std::cout << append(a, IntList {5, -1, 10, 2, 8}) << "\n";
+    std::cout << repeat(10, 1) << "\n";
+    std::cout << merge(IntList {1, 2, 3}, IntList {0, 2, 4, 5, 6}) << "\n";
 
     return 0;
 }
--- 10. Recursion/recursion/recursion.cpp
+++ 10. Recursion/recursion/recursion.cpp
@@ -1,7 +1,7 @@
 #include "intlist.h"
 #include "recursion.h"
 
-// Complejidad: O(N)
+// Complexity: O(N)
 int size(const IntList& a)
 {
     if (is_empty(a)) {
@@ -11,7 +11,7 @@ int size(const IntList& a)
     }
 }
 
-// Complejidad: O(N)
+// Complexity: O(N)
 IntList cons_end(int value, const IntList& a)
 {
     if (is_empty(a)) {
@@ -21,7 +21,7 @@ IntList cons_end(int value, const IntList& a)
     }
 }
 
-// Complejidad: O(N)
+// Complexity: O(N)
 int sum(const IntList& a)
 {
     if (is_empty(a)) {
@@ -31,34 +31,64 @@ int sum(const IntList& a)
     }
 }
 
+// Complexity: O(N)
 IntList duplicate(const IntList& a)
 {
-    return IntList {};
+    if (is_empty(a)) {
+        return IntList {};
+    } else {
+        return cons(first(a), cons(first(a), duplicate(rest(a))));
+    }
 }
 
+// Complexity: O(N)
 int last(const IntList& a)
 {
-    return 0;
+    if (is_empty(rest(a))) {
+        return first(a);
+    } else {
+        return last(rest(a));
+    }
 }
 
+// Complexity: O(N)
 IntList but_last(const IntList& a)
 {
-    return IntList {};
+    if (is_empty(rest(a))) {
+        return IntList {};
+    } else {
+        return cons(first(a), but_last(rest(a)));
+    }
 }
 
+// Complexity: O(N)
 int maximum(const IntList& a)
 {
-    return 0;
+    if (is_empty(rest(a))) {
+        return first(a);
+    } else {
+        return std::max(first(a), maximum(rest(a)));
+    }
 }
 
+// Complexity: O(N), where N is the size of a
 IntList append(const IntList& a, const IntList& b)
 {
-    return IntList {};
+    if (is_empty(a)) {
+        return b;
+    } else {
+        return cons(first(a), append(rest(a), b));
+    }
 }
 
+// Complexity: O(N)
 IntList repeat(int n, int value)
 {
-    return IntList {};
+    if (n == 0) {
+        return IntList {};
+    } else {
+        return cons(value, repeat(n - 1, value));
+    }
 }
 
 IntList reverse(const IntList& a)
@@ -66,9 +96,20 @@ IntList reverse(const IntList& a)
     return IntList {};
 }
 
+
+// Complexity: O(N), where N is the sum of the sizes of both lists a and b
+//             O(N + M), where N is the size of a, and M is the size of b
 IntList merge(const IntList& a, const IntList& b)
 {
-    return IntList {};
+    if (is_empty(a)) {
+        return b;
+    } else if (is_empty(b)) {
+        return a;
+    } else if (first(a) < first(b)) {
+        return cons(first(a), merge(rest(a), b));
+    } else {
+        return cons(first(b), merge(a, rest(b)));
+    }
 }
 
 bool is_prefix(const IntList& a, const IntList& b)
