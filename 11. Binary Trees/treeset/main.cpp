735e74e820f83606217a2f40f52bfbf3b65a10f1
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-11-23 22:32:48 +0000

    Add code for tree traversals

--- 11. Binary Trees/treeset/main.cpp
+++ 11. Binary Trees/treeset/main.cpp
@@ -2,7 +2,46 @@
 #include <vector>
 #include "treeset.h"
 
+void print_value(int x)
+{
+    std::cout << x << " ";
+}
+
 int main()
 {
+    TreeSet<int> a;
+    a.add(15);
+    a.add(8);
+    a.add(20);
+    a.add(23);
+    a.add(21);
+    a.add(3);
+    a.add(1);
+    a.add(9);
+
+    a.inorder(print_value);
+    std::cout << "\n";
+
+    a.preorder(print_value);
+    std::cout << "\n";
+
+    a.postorder(print_value);
+    std::cout << "\n";
+
+    a.inorder([&] (int x) { std::cout << x << "\n"; });
+    std::cout << "\n";
+
+    int sum = 0;
+    a.preorder([&] (int x) { sum += x; });
+    std::cout << "sum = " << sum << "\n";
+
+    int even = 0;
+    a.postorder([&] (int x) {
+        if (x % 2 == 0) {
+            ++even;
+        }
+    });
+    std::cout << "even = " << even << "\n";
+
     return 0;
 }
--- 11. Binary Trees/treeset/treeset.h
+++ 11. Binary Trees/treeset/treeset.h
@@ -11,7 +11,7 @@ public:
 
     TreeSet() {}
 
-    //
+    // Complexity: O(log N)
     bool add(T value)
     {
         if (_root) {
@@ -44,6 +44,21 @@ public:
         }
     }
 
+    void inorder(std::function<void(T)> fn) const
+    {
+        _inorder(fn, _root);
+    }
+
+    void preorder(std::function<void(T)> fn) const
+    {
+        _preorder(fn, _root);
+    }
+
+    void postorder(std::function<void(T)> fn) const
+    {
+        _postorder(fn, _root);
+    }
+
 private:
 
     struct Node {
@@ -57,6 +72,33 @@ private:
         Node* left = nullptr;
         Node* right = nullptr;
     };
+
+    void _inorder(std::function<void(T)> fn, Node* p) const
+    {
+        if (p) {
+            _inorder(fn, p->left);
+            fn(p->value);
+            _inorder(fn, p->right);
+        }
+    }
+
+    void _preorder(std::function<void(T)> fn, Node* p) const
+    {
+        if (p) {
+            fn(p->value);
+            _preorder(fn, p->left);
+            _preorder(fn, p->right);
+        }
+    }
+
+    void _postorder(std::function<void(T)> fn, Node* p) const
+    {
+        if (p) {
+            _postorder(fn, p->left);
+            _postorder(fn, p->right);
+            fn(p->value);
+        }
+    }
 
     Node* _root = nullptr;
     int _size = 0;
